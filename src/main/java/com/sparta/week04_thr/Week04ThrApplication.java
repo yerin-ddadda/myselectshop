c29fc54d4a6b832071bf4140c035376ba14bff92
yerin <yerin0110@naver.com>
2022-04-01 23:57:36 +0900

    Initial commit

---
+++ src/main/java/com/sparta/week04_thr/Week04ThrApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.week04_thr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week04ThrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week04ThrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/week04_thr/Week04ThrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.week04_thr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week04ThrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
